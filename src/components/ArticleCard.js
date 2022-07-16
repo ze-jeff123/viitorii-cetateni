2d50e8657590f00c676781831dd453119b967ace
Tudor <pagu.tudor@gmail.com>
2022-07-16 12:41:54 +0300

    Added ArticleCard

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,24 +1,41 @@
 import React from 'react'
 import Article from './Article'
 import { CardMedia, Typography, CardContent, Card } from '@mui/material'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { slugifyArticle } from '../global/articlesUtility'
+const StyledCard = styled(Card)`
+    max-width : 345px;
+    &:hover {
+        filter : brightness(0.8);
+        cursor : pointer;
+    }
+`
+const StyledLink = styled(Link)`
+    text-decoration : none;
+    color : black;
+`
+
 function ArticleCard({ article }) {
     return (
-        <Card>
-            <CardMedia
-                component="img"
-                height="140"
-                image={article.image}
-                alt={article.featuredImageAlt || "imagine pentru articol"}
-            />
-            <CardContent>
-                <Typography gutterBottom variant="h5" component='div'>
-                    {article.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                    {article.excerpt || article.content} 
-                </Typography>
-            </CardContent>
-        </Card>
+        <StyledLink to={`/articole/${slugifyArticle(article)}`}>
+            <StyledCard>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={article.featuredImage}
+                    alt={article.featuredImageAlt || "imagine pentru articol"}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component='div'>
+                        {article.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {article.excerpt || article.content}
+                    </Typography>
+                </CardContent>
+            </StyledCard>
+        </StyledLink>
     )
 }
 

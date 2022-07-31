75ed4bf4bd56295a72140496677d3d4aa5e72605
Tudor <pagu.tudor@gmail.com>
2022-07-31 19:47:51 +0300

    new 3

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,36 +1,9 @@
-import { BrowserRouter,HashRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
-import Articles from "./components/Articles";
-import { getCategories, slugifyArticle } from "./global/articlesUtility";
-import { useEffect, useState } from 'react';
-import importAllDefault from "./global/importAllDefaults";
-import Home from "./components/Home";
+
 const RouteSwitch = () => {
-
-  const [articles, setArticles] = useState(null);
-  useEffect(() => {
-    importAllDefault().then((val) => { setArticles(val) });
-  }, []);
-
-
-  articles&&
-  articles.forEach((article) => {
-    console.log(`/articole/${slugifyArticle(article)}`);
-  });
-
-
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='/articole' element={<Articles />} />
-        {
-          articles &&
-          articles.map((article) => (
-            <Route path={`/articole/${slugifyArticle(article)}`} element={<Articles />} />))
-        }
-      </Routes>
-    </HashRouter>
+    <div>
+      hi
+    </div>
   );
 };
 

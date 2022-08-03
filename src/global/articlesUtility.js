39784d9fb28cdbfd9dce77b8f7236212ac6261ba
Tudor <pagu.tudor@gmail.com>
2022-08-03 16:06:43 +0300

    add articles utility

---
+++ src/global/articlesUtility.js
@@ -0,0 +1,16 @@
+function makeUnique(array) {
+    return [...new Set(array)];
+}
+
+function getCategories(articles) {
+    return articles && makeUnique(articles.map((article) => article.category));
+}
+
+function slugify(title) {
+    return title.toLowerCase().split('').map((char) => char === ' ' ? '-' : char).filter((char) => (char !== '?')).join('');
+}
+
+function slugifyArticle(article) {
+    return `${slugify(article.category)}/${slugify(article.title)}`;
+}
+export { getCategories, slugifyArticle , slugify};
